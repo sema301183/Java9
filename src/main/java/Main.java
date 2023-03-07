a298ceeb0d1c6d7783de33e56e4f4be8e7394460
Sema test <ganzalovdmitriy@gmail.com>
2023-03-07 20:47:07 +0300

    .gitignore

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         Post post = new Post();
+        post.birthday = new FormDate();
         post.name = "Иван";
+        post.birthday.day = 9;
+        post.birthday.month = 3;
+        post.birthday.year = 1934;
         post.passport = "4444 №444444";
         post.patronymic = "Иванович";
         post.phone = "+79999999999";
         post.surname = "Иванов";
         post.subscription = String.valueOf(true);
-
 
         System.out.println(post);
     }
